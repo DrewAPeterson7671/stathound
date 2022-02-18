33a4787b001df8a7c44a68cb1604b633397c0783
Drew Peterson <drew.a.peterson@hotmail.com>
2022-02-18 15:18:32 -0800

    Continue work on refining get_player_info and refactor for an array.  Create display_player_info method to verify results.  Add notes on needed improvments

--- app/helpers/scraper_helper.rb
+++ app/helpers/scraper_helper.rb
@@ -35,24 +35,139 @@ module ScraperHelper
     @doc_season = target_scrape("http://basketball-reference.com/leagues/#{league_year}_adj_shooting.html")
   end
 
-  def gather_players_from_season
+  def player_list_from_season
     @name_list = []
-    player_season_table = @doc_season.css("tbody") # works on console!!
+    player_season_table = @doc_season.css("tbody")
     player_season_table.search('tr.thead').each(&:remove)
     rows = player_season_table.css("tr")
-    puts "rows count " + rows.count.to_s
-    puts rows[0].at_css("td").try(:text)
-    puts rows[0].at_css("a").attributes["href"].try(:value)
     rows.each do |r|
-      @name_list << r.at_css("td").try(:text)
+      # @name_list << r.at_css("td").try(:text)
       @name_list << r.at_css("a").attributes["href"].try(:value)
     end
     @name_list = @name_list.uniq
-    puts @name_list.count
-    # trade duplicates included, need to filter out
+    # In 1947 should yield 161 players
   end
+
+  def gather_players_from_season
+    @name_list.each do |nl|
+      get_player_page(nl)
+    end
+  end
+
+  def get_player_page(player_url)
+    @player_page = Nokogiri::HTML.parse(URI.open("http://basketball-reference.com#{player_url}"))
+  end
+
+  def get_player_info # Not yet attempted
+    # This page has a click to show that may be an issue
+    @info = @player_page.css("div#info")
+    @player_info = []
+
+    @player_info << @info.at_css("span").try(:text)
+    @info.at('p:contains("Pronunciation:")') ? @player_info << @info.at('p:contains("Pronunciation:")').children.try(:text).gsub("Pronunciation:", "").gsub("\n", "").gsub(" ", "").strip : @player_info << "None"
+    ## Mutumbo has "\\" before and after.  Keep?
+    ## if statement doesn't seem to work
+    @player_info << @info.at_css("strong").try(:text).gsub("\n  ", "")
+    # Mutumbo took "Pronunciation" instead of full name
+    @player_info << @info.at_css("p:nth-child(3)").try(:text).gsub("\n", "").gsub("(", "").gsub(")", "")
+    # on Motumbo gives his full name and includes twitter
+    # Test Existence if it contains Position or (born which is class="desc"
+    # Check what this child would yield if no nickname, then check for that
+    # Nickname is causing the nth-child number to shift on non-nickname players
+    # Motumbo has a (born... above his nickname - test for that
+    @player_position_shoot = @info.at('p:contains("Position:")').try(:text).gsub("\n", "").gsub("  ", "").split("▪")
+    @player_info << @player_position_shoot[0].gsub("Position:", "")
+    @player_info << @player_position_shoot[1].gsub("Shoots:", "") if @info.at('p:contains("Shoots:")')
+    @player_info << @info.at("//span[@itemprop = 'height']").children.try(:text)
+    @player_info << @info.at("//span[@itemprop = 'weight']").children.try(:text)
+    @player_info << @info.at("//span[@itemprop = 'birthDate']").children.try(:text).gsub("\n   ", "").strip.squeeze(" ")
+    @player_info << @info.at("//span[@itemprop = 'birthPlace']").children.try(:text).gsub("\n    in ", "")
+    @player_info << @info.at('p:contains("College:")').children.try(:text).gsub("College:", "").gsub("\n", "").strip if @info.at('p:contains("College:")')
+    @player_info << @info.at('p:contains("High School:")').children.try(:text).gsub("High School:", "").gsub("\n", "").strip if @info.at('p:contains("High School:")')
+    @info.at('p:contains("Recruiting Rank:")') ? @player_info << @info.at('p:contains("Recruiting Rank:")').children.try(:text).gsub("Recruiting Rank:", "").gsub("\n", "").strip : @player_info << "None"
+    @info.at('p:contains("Draft:")') ? @player_info << @info.at('p:contains("Draft:")').children.try(:text).gsub("Draft:", "").gsub("\n", "").gsub(" ", "").strip : @player_info << "Undrafted"
+    @player_info << @info.at('p:contains("NBA Debut:")').children.try(:text).gsub("NBA Debut:", "").gsub("\n", "").strip
+    # Connie Hawkins had ABA debut, messed up NBA debut 
   
+    @player_info << @info.at('p:contains("Career Length:")').children.try(:text).gsub("Career Length:", "").gsub("\n", "").gsub(" ", "").strip if @info.at('p:contains("Career Length:")')
+    # on Durant, gave Ben Wallace numbers
+
+    @player_info << @info.at('p:contains("Experience:")').children.try(:text).gsub("Experience:", "").gsub("\n", "").gsub(" ", "").strip if @info.at('p:contains("Experience:")')
+
+    ## Review each line to ensure SOMETHING is pushed
+
+    ## Relatives by text?
+    ## capture image? or image URL?
+    ## Pronunciation missing SEE above
+    ## modern HOF player inconsistencies and may not all exist
+    ### on Durant, need to grab honors 12x all-star, 4x scoring champ, etc
+    ### Durant doesnt have all the honors such as most improved and DPOY and 50th or HOF
+    ### should mark if player is active as of scraping
+
+    # QUESTION - will @Player variables  
+
+  end
+
+  def display_player_info
+    player_info_template = ["player_name", "pronunciation", "full_name", "nickname", "position", "shoots", "height", "weight", "birthdate", "birthplace", "college", "high_school", "college_recruiting_rank", "draft", "nba_debut", "career_length", "experience"]
+
+    @player_info.each_with_index do |pi, i|
+      puts player_info_template[i] + " == " + pi
+    end
+  end
+
+  def get_player_reg_season_total
+    @reg_total = @player_page.css("table#totals")
+    ## for all tables, import the headers to verify stats line up first between leagues-eras
+    # grab career totals to compare for validation?
+
+  end
+
+  def open_link(css_link_class)
+    # currently doesn't work
+    href = @player_page.css("." + css_link_class).first.children.first['href']
+    new_url = URI.join @player_page, href
+    @player_playoff_page = open(new_url)
+  end
+
+  def get_player_playoff_total
+    open_link('.sr_preset')
+    @playoff_totals = @player_page.css("table#playoffs_totals")
+  end
+
+  def get_player_reg_season_per100
+  end
+
+  def get_player_playoff_per100
+  end
+
+  def get_player_reg_season_advanced
+  end
+
+  def get_player_playoff_advanced
+  end
+
+  def get_player_reg_season_adj_shooting
+  end
+
+  def get_player_reg_season_playbyplay
+  end
+
+  def get_player_playoff_playbyplay
+  end
+
+  def get_player_reg_season_shooting
+  end
+
+  def get_player_playoff_shooting
+  end
+
 end
+
+
+    # @player_name = @info.at_css("span:nth-child(1)").text
+    # @player_name = @info.at_css("span:nth-child(2)").text
+
 
     # player_season_table = @doc_season.css("tbody") works on console!!
 
--- lib/notes_script.rb
+++ lib/notes_script.rb
@@ -116,3 +116,158 @@ warriors_player_images = warriors_roster.map do |player|
    doc = Nokogiri::HTML.parse(browser.html) 
    doc.at_css("div#ires img")["src"] 
  end 
+
+ # click on playoff page
+
+require 'open-uri'
+require 'nokogiri'
+require 'uri'
+
+base_url = "https://www.google.com/search?q=stackoverflow"
+document = open(base_url)
+parsed_content = Nokogiri::HTML(document.read)
+href = parsed_content.css('.r').first.children.first['href']
+new_url = URI.join base_url, href
+new_document = open(new_url)
+
+# translated
+
+href = @player_page.css('.sr_preset').first.children.first['href']
+new_url = URI.join @player_page, href
+@player_playoff_page = open(new_url)
+
+@player_page = Nokogiri::HTML.parse(URI.open("http://basketball-reference.com/players/d/duranke01.html"))
+
+/players/a/abramjo01.html
+/players/f/fleisje01.html
+/players/h/hawkico01.html
+/players/m/mutomdi01.html
+/players/m/millebr01.html
+/players/w/wallabe01.html
+/players/d/duncati01.html
+
+/players/j/jordami01.html
+/players/j/jamesle01.html
+
+@player_page = Nokogiri::HTML.parse(URI.open("http://basketball-reference.com/players/a/abramjo01.html"))
+@player_page = Nokogiri::HTML.parse(URI.open("http://basketball-reference.com/players/f/fleisje01.html"))
+@player_page = Nokogiri::HTML.parse(URI.open("http://basketball-reference.com/players/h/hawkico01.html"))
+@player_page = Nokogiri::HTML.parse(URI.open("http://basketball-reference.com/players/m/mutomdi01.html"))
+@player_page = Nokogiri::HTML.parse(URI.open("http://basketball-reference.com/players/d/duncati01.html"))
+@player_page = Nokogiri::HTML.parse(URI.open("http://basketball-reference.com/players/m/millebr01.html"))
+@player_page = Nokogiri::HTML.parse(URI.open("http://basketball-reference.com/players/w/wallabe01.html"))
+@player_page = Nokogiri::HTML.parse(URI.open("http://basketball-reference.com/players/d/duranke01.html"))
+
+
+player_info_template = ["player_name", "pronunciation", "full_name", "nickname", "position", "shoots", "height", "weight", "birthdate", "birthplace", "college", "high_school", "college_recruiting_rank", "draft", "nba_debut", "career_length", "experience"]
+@player_info.each do |pi| 
+   puts "Player Name "
+end
+
+# for image url Durant - no image for John Abramovic or Jerry Fleishman
+<img itemscope="image" src="https://www.basketball-reference.com/req/202106291/images/players/duranke01.jpg" alt="Photo of Kevin Durant">
+
+# relatives
+<p><strong>Relatives</strong>: Nephew&nbsp;<a itemprop="relatedTo" href="/players/k/kabenmf01.html">Mfiondu&nbsp;Kabengele</a> </p>
+
+# main name more specificity
+<h1 itemprop="name">
+		<span>Dikembe Mutombo</span>
+</h1>
+<p>
+  <strong>
+  <strong>Dikembe Mutombo</strong>
+  </strong>
+    
+  ▪
+  <a href="/friv/twitter.html">Twitter</a>:
+  <a href="https://twitter.com/officialmutombo">officialmutombo</a>
+</p>
+<p>
+   <span class="desc">(born Dikembe Mutombo Mpolondo Mukamba Jean-Jacques Wamutombo)</span>
+</p>
+<p>
+(Deke, Mt. Mutombo)
+</p>
+<p>
+  <strong>
+  Position:
+  </strong>
+  Center
+
+
+  
+  ▪
+  
+  <strong>
+  Shoots:
+  </strong>
+  Right
+</p>
+
+# Connie Hawkins ABA
+
+<p>
+  <strong>NBA Debut: </strong><a href="/boxscores/196910160PHO.html">October 16, 1969</a>&nbsp;▪&nbsp;<strong>ABA Debut: </strong><a href="/boxscores/196710230NJA.html">October 23, 1967</a>
+</p>
+
+# Achievements
+<li class="important special"><a>Hall of Fame</a></li>
+<li class="all_star"><a>15x All Star</a></li>
+<li class=""><a>5x NBA Champ</a></li>
+<li class=""><a>15x All-NBA</a></li>
+<li class=""><a>15x All-Defensive</a></li>
+<li class=""><a>1997-98 All-Rookie</a></li>
+<li class=""><a>1997-98 ROY</a></li>
+<li class=""><a>3x Finals MVP</a></li>
+<li class=""><a>1999-00 AS MVP</a></li>
+<li class=""><a>2x MVP</a></li>
+<li class=""><a>NBA 75th Anniv. Team</a></li>
+
+# Mutumbo
+<li data-tip="2x NBA TRB Champ" class=" poptip"><a>2x TRB Champ</a></li>
+<li data-tip="3x NBA BLK Champ" class=" poptip"><a>3x BLK Champ</a></li>
+<li class=""><a>4x Def. POY</a></li> 
+
+#Aaron Mckie /players/m/mckieaa01.html
+<li class=""><a>2000-01 Sixth Man</a></li>
+
+# Durant
+<li data-tip="4x NBA Scoring Champ" class=" poptip"><a>4x Scoring Champ</a></li>
+
+# Steve Nash /players/n/nashst01.html
+<li data-tip="5x NBA AST Champ" class=" poptip"><a>5x AST Champ</a></li>
+
+#Julius Randle /players/r/randlju01.html
+<li class=""><a>2020-21 Most Improved</a></li>
+
+# Jimmy Butler /players/b/butleji01.html
+<li data-tip="2020-21 NBA STL Champ" class=" poptip"><a>2020-21 STL Champ</a></li>
+
+
+
+
+
+
+
+@player_name
+@player_pronunciation
+@player_full_name
+@player_nickname
+@player_position
+@player_shoots
+@player_height
+@player_weight
+@player_birthdate
+@player_birthplace
+@player_college
+@player_high_school
+@player_college_recruiting_rank
+@player_draft
+@player_nba_debut
+@player_career_length
+@player_experience
+
+
+
+@info.at('strong:contains("Position")').next_element.text.strip
